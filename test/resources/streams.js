3240f5b029d428020e8e446c9a47728c5a1899bc
Rohitt Vashishtha <aero31aero@gmail.com>
2017-11-29 03:13:14 +0530

    tests: Make streams.js tests standalone.

--- test/resources/streams.js
+++ test/resources/streams.js
@@ -1,22 +1,94 @@
 const streams = require('../../lib/resources/streams');
+const common = require('../common');
 const chai = require('chai');
 chai.use(require('chai-as-promised'));
 
-const realm = process.env.ZULIP_REALM;
-const apiURL = `${realm}/api/v1`;
+chai.should();
 
 const config = {
-  username: process.env.ZULIP_USERNAME,
-  apiKey: process.env.ZULIP_API_KEY,
-  apiURL,
+  username: 'valid@email.com',
+  apiKey: 'randomcharactersonlyq32YIpC8aMSH',
+  apiURL: 'valid.realm.url/api/v1',
 };
 
-chai.should();
 describe('Streams', () => {
-  it('Should fetch streams', () => {
+  it('should fetch streams', () => {
+    const validator = (url, options) => {
+      url.should.contain(`${config.apiURL}/streams`);
+      options.should.not.have.property('body');
+    };
+    const output = {
+      result: 'success',
+      msg: '',
+      streams: [{
+        name: 'Denmark',
+        stream_id: 1,
+        invite_only: false,
+        description: 'A Scandinavian country',
+      }, {
+        name: 'Rome',
+        stream_id: 2,
+        invite_only: false,
+        description: 'Yet another Italian city',
+      }, {
+        name: 'Scotland',
+        stream_id: 3,
+        invite_only: false,
+        description: 'Located in the United Kingdom',
+      }, {
+        name: 'Venice',
+        stream_id: 4,
+        invite_only: false,
+        description: 'A northeastern Italian city',
+      }, {
+        name: 'Verona',
+        stream_id: 5,
+        invite_only: false,
+        description: 'A city in Italy',
+      }],
+    };
+    const stubs = common.getStubs(validator, output);
     streams(config).retrieve().should.eventually.have.property('result', 'success');
+    common.restoreStubs(stubs);
   });
-  it('Should fetch subscriptions', () => {
+  it('should fetch subscriptions', () => {
+    const validator = (url, options) => {
+      url.should.contain(`${config.apiURL}/users/me/subscriptions`);
+      options.should.not.have.property('body');
+    };
+    const output = {
+      msg: '',
+      result: 'success',
+      subscriptions: [{
+        color: '#e79ab5',
+        invite_only: false,
+        desktop_notifications: true,
+        subscribers: [Object],
+        stream_id: 1,
+        pin_to_top: false,
+        email_address: 'Denmark+986326cbbaef74fcb4c77cc41d47b12c@zulipdev.com:9991',
+        audible_notifications: true,
+        description: 'A Scandinavian country',
+        in_home_view: true,
+        push_notifications: false,
+        name: 'Denmark',
+      }, {
+        color: '#e79ab5',
+        invite_only: false,
+        desktop_notifications: true,
+        subscribers: [Object],
+        stream_id: 3,
+        pin_to_top: false,
+        email_address: 'Scotland+a3a2dc96b0406d47c826041f773ee29a@zulipdev.com:9991',
+        audible_notifications: true,
+        description: 'Located in the United Kingdom',
+        in_home_view: true,
+        push_notifications: false,
+        name: 'Scotland',
+      }],
+    };
+    const stubs = common.getStubs(validator, output);
     streams(config).subscriptions.retrieve().should.eventually.have.property('result', 'success');
+    common.restoreStubs(stubs);
   });
 });
