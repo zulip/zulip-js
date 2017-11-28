a62034d9640da5112aaf7ab525168d0141b101fe
Rohitt Vashishtha <aero31aero@gmail.com>
2017-11-29 04:09:44 +0530

    tests: Make events.js tests standalone.

--- test/resources/events.js
+++ test/resources/events.js
@@ -1,30 +1,43 @@
 const events = require('../../lib/resources/events');
-const queues = require('../../lib/resources/queues');
+const common = require('../common');
 const chai = require('chai');
-
 chai.use(require('chai-as-promised'));
-
-const realm = process.env.ZULIP_REALM;
-const apiURL = `${realm}/api/v1`;
-const config = {
-  username: process.env.ZULIP_USERNAME,
-  apiKey: process.env.ZULIP_API_KEY,
-  apiURL,
-};
-
-const params = {
-  last_event_id: -1,
-  dont_block: true,
-};
 
 chai.should();
 
-describe('Events', () => {
-  before(() => queues(config).register({ event_types: ['message'] }).then((res) => {
-    params.queue_id = res.queue_id;
-  }));
+const config = {
+  username: 'valid@email.com',
+  apiKey: 'randomcharactersonlyq32YIpC8aMSH',
+  apiURL: 'valid.realm.url/api/v1',
+};
 
-  it('Should fetch events', () => {
+describe('Events', () => {
+  it('should fetch events', () => {
+    const params = {
+      last_event_id: -1,
+      dont_block: true,
+    };
+    const validator = (url, options) => {
+      url.should.contain(`${config.apiURL}/events`);
+      options.should.not.have.property('body');
+      const urldata = url.split('?', 2)[1].split('&'); // URL: host/messages?key=value&key=value...
+      urldata.length.should.equal(2);
+      urldata.should.contain(`last_event_id=${params.last_event_id}`);
+      urldata.should.contain(`dont_block=${params.dont_block}`);
+    };
+    const output = {
+      events: [{
+        id: 0,
+        message: [Object],
+        type: 'message',
+        flags: [Object],
+      }],
+      result: 'success',
+      msg: '',
+      queue_id: '1511901550:3',
+    };
+    const stubs = common.getStubs(validator, output);
     events(config).retrieve(params).should.eventually.have.property('result', 'success');
+    common.restoreStubs(stubs);
   });
 });
