d745dd736aae061e55313011eca4ab29167d7508
Arpith Siromoney <arpith@feedreader.co>
2018-04-17 21:55:24 -0400

    api: Use the new reactions API.
    1. the url is /messages/<message_id>/reactions
    2. the params are emoji_name, emoji_code and reaction_type
    3. post and delete require emoji_code

--- src/resources/reactions.js
+++ src/resources/reactions.js
@@ -1,8 +1,12 @@
 const api = require('../api');
 
 function reactions(config) {
-  const url = `${config.apiURL}/reactions`;
-  const call = (method, params) => api(url, config, method, params);
+  const url = messageID => `${config.apiURL}/messages/${messageID}/reactions`;
+  const call = (method, initParams) => {
+    const params = Object.assign({}, initParams);
+    delete params.message_id;
+    return api(url(initParams.message_id), config, method, params);
+  };
   return {
     add: params => call('POST', params),
     remove: params => call('DELETE', params),
--- test/resources/reactions.js
+++ test/resources/reactions.js
@@ -9,14 +9,17 @@ describe('Reactions', () => {
   it('should add reaction to message', (done) => {
     const params = {
       message_id: 1,
-      emoji: 'smile',
+      emoji_name: 'musical_note',
+      emoji_code: '1f3b5',
+      reaction_type: 'unicode_emoji',
     };
     const validator = (url, options) => {
-      url.should.equal(`${common.config.apiURL}/reactions`);
+      url.should.equal(`${common.config.apiURL}/messages/${params.message_id}/reactions`);
       options.method.should.be.equal('POST');
-      Object.keys(options.body.data).length.should.equal(2);
-      options.body.data.message_id.should.equal(params.message_id);
-      options.body.data.emoji.should.equal(params.emoji);
+      Object.keys(options.body.data).length.should.equal(3);
+      options.body.data.emoji_name.should.equal(params.emoji_name);
+      options.body.data.emoji_code.should.equal(params.emoji_code);
+      options.body.data.reaction_type.should.equal(params.reaction_type);
     };
     const output = {
       result: 'success',
@@ -32,11 +35,15 @@ describe('Reactions', () => {
   });
 
   it('should remove reaction from message', (done) => {
-    const messageID = 1;
-    const emoji = 'smile';
-    const params = { message_id: messageID, emoji };
+    const params = {
+      message_id: 1,
+      emoji_code: '1f3b5',
+      reaction_type: 'unicode_emoji',
+    };
     const validator = (url, options) => {
-      url.should.equal(`${common.config.apiURL}/reactions?message_id=${messageID}&emoji=${emoji}`);
+      const path = `${common.config.apiURL}/messages/${params.message_id}/reactions`;
+      const query = `emoji_code=${params.emoji_code}&reaction_type=${params.reaction_type}`;
+      url.should.equal(`${path}?${query}`);
       options.method.should.be.equal('DELETE');
       options.should.not.have.property('body');
     };
