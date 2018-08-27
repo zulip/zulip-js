daa7cf17689fc18055e32ecc2fc45a777d629ae7
Anupam-dagar <dagaranupam@gmail.com>
2018-08-27 11:42:55 +0530

    Add endpoint to delete reactions on a message by message id.

--- examples/messages.js
+++ examples/messages.js
@@ -66,3 +66,10 @@ zulip(config).then((z) => {
   };
   z.messages.getById(readParams).then(console.log);
 }).catch(err => console.log(err.message));
+
+zulip(config).then((z) => {
+  const readParams = {
+    message_id: 1,
+  };
+  z.messages.deleteReactionById(readParams).then(console.log);
+}).catch(err => console.log(err.message));
